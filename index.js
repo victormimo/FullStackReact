59c8d757c619c00fdcb9169b62bd453c9eb1ce2b
Victor Mimo <victormimo@mys-MacBook.local>
2018-09-18 16:38:39 -0400

    finished billing and setting production build

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express"); // like from express import express
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser"); //middleware therefore connected by app.use
+const keys = require("./config/keys");
+
 require("./models/User");
 require("./services/passport.js");
 
 mongoose.connect(keys.mongoURI); //url to db
 const app = express();
 
+app.use(bodyParser.json()); //req.body is how this will be dispayed//middleware
+
 app.use(
+  //middleware
   cookieSession({
     maxAge: 2592000000,
     keys: [keys.cookieKey]
@@ -20,6 +25,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+//changes for production
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets
+  //ie main.js file
+  app.use(express.static("client/build")); //ie if the route is not recognized, look into client/build
+
+  //express will handle index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 /* dynamic port binding */
 const PORT = process.env.PORT || 4000; // runtime variable, ie when its beginnning to be executed, heroku gives port. however only fully works in prod. if in dev use static port
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose; // same as const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   //while schemaless mongoose requires you to declare all attributes
-  googleID: String // ie all our model instances so far only have googleid
+  googleID: String, // ie all our model instances so far only have googleid
+  credits: { type: Number, default: 0 } //when defining schema you can either define type or a custom one through an object
 });
 
 mongoose.model("users", userSchema); // creates model class, w 2 arguments youll pass it to monggose, w 1 youll read it?
